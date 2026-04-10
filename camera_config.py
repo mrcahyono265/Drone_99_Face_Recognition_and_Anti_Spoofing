bad567480e1cd6e60e27b9ab7c2084f90634256e
Mohammad Ridho Cahyono <mohammadridhocahyono265@gmail.com>
2026-04-10 20:30:42 +0700

    optimize camera_config.py

--- camera_config.py
+++ camera_config.py
@@ -1,46 +1,87 @@
 import cv2
 import time
+import threading
+import os
 from datetime import datetime
 
-# URL RSTP Drone E99
-RTSP_URL = "rtsp://192.168.1.1:7070/webcam"
+os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;udp|fflags;nobuffer|flags;low_delay"
 
-# Initialize connection with FFMPEG
-cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
-# Configure buffer size (Avoiding Lagging)
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+class cameraDroneThread:
+    def __init__(self, rtsp_url):
+        print("[INFO]: Connecting drone...")
+        self.stream = cv2.VideoCapture(rtsp_url, cv2.CAP_FFMPEG)
+        self.stream.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        
+        # read first frame
+        (self.grabbed, self.frame) = self.stream.read()
+        self.stopped = False
 
-print("[INFO]: Connecting to the drone's camera...")
-time.sleep(2.0)
+    def start(self):
+        # Thread starting
+        threading.Thread(target=self.update, args=(), daemon=True).start()
+        return self
 
-# Varible to count frames
-prev_time = time.time()
-fps_smoothed = 0
+    def update(self):
+        # Looping
+        while True:
+            if self.stopped:
+                self.stream.release()
+                return
+            # read next frame
+            self.grabbed, self.frame = self.stream.read()
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        print("[ERROR]: Unconnected from the drone's camera. Check the connection")
-        break
+    def read(self):
+        # Call looping 
+        if self.frame is not None:
+            return cv2.rotate(self.frame, cv2.ROTATE_90_CLOCKWISE)
+        return None
 
-    # Rotating camera 90 degrees
-    frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    def stop(self):
+        self.stopped = True
 
-    # Adding timestamp to the frame
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    cv2.putText(frame, current_time, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
+
+def main():
+    # URL RTSP drone camera
+    RTSP_URL = "rtsp://192.168.1.1:7070/webcam"
     
-    # Calculate dan show FPS
-    time_diff = time.time() - prev_time
-    if time_diff > 0:
-        fps_smoothed = (fps_smoothed * 0.9) + ((1 / time_diff) * 0.1) if fps_smoothed > 0 else (1 / time_diff)
+    # Initialize and start
+    camera = cameraDroneThread(RTSP_URL).start()
+    time.sleep(2.0)
+
     prev_time = time.time()
-    cv2.putText(frame, f"FPS: {int(fps_smoothed)}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
+    fps_smoothed = 0
 
-    cv2.imshow("Drone Camera", frame)
+    print("[INFO] Running...")
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    while True:
+        frame = camera.read()
+        
+        if frame is None:
+            continue
 
-cap.release()
-cv2.destroyAllWindows()
+        # Display timestamp
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cv2.putText(frame, current_time, (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 1)
+        
+        # Count FPS
+        time_diff = time.time() - prev_time
+        if time_diff > 0:
+            fps_smoothed = (fps_smoothed * 0.9) + ((1 / time_diff) * 0.1) if fps_smoothed > 0 else (1 / time_diff)
+        prev_time = time.time()
+        
+        # Display FPS
+        cv2.putText(frame, f"FPS: {int(fps_smoothed)}", (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+
+        # Show App
+        cv2.imshow("Drone E99 Face Recognition and Anti Spoofing", frame)
+
+        frame_count += 1
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    camera.stop()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
